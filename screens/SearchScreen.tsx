8056bdb8a0c215a5b85e1e487c87546e4200d516
Bright Atsighi <brytebee@gmail.com>
2024-07-26 06:27:00 +0100

    Use hook in search screen

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,36 +1,21 @@
 import {View, Text} from 'react-native';
 import SearchBar from '../components/Search';
 import {useState} from 'react';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 export default function SearchScreen() {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errMessage, setErrMessage] = useState('');
-
-  const searchResult = async () => {
-    setErrMessage('');
-    try {
-      const res = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term,
-          location: 'san jose',
-        },
-      });
-      setResults(res.data.businesses);
-    } catch (err) {
-      setErrMessage('Something went wrong fetching businesses');
-    }
-  };
+  const [searchApi, errMessage, results] = useResults();
 
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={setTerm}
-        onTermSubmit={searchResult}
+        // @ts-ignore
+        onTermSubmit={() => searchApi(term)}
       />
+      {/* @ts-ignore */}
       {errMessage ? <Text>{errMessage}</Text> : null}
       <Text>We found {results.length} results.</Text>
     </View>
